dbb3b8b6e8996a5e404d17f582346a4113de7bd0
Sheryl HG <sherylhdzguevara@gmail.com>
2024-05-07 14:15:06 -0600

    mobile menu y carrito de compras

--- main.js
+++ main.js
@@ -1,8 +1,45 @@
 const navEmail = document.querySelector('.navbar-email');
+const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
+function asideClosed() {
+   const isAsideClosed = aside.classList.contains('inactive');
+
+
+   if (!isAsideClosed) {
+      aside.classList.add('inactive');
+   }
+}
 
 function toggleDesktopMenu(){
+   asideClosed();
    desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+   asideClosed();
+   mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside(){
+   const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+   const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+
+   if (!isMobileMenuClosed) {
+      mobileMenu.classList.add('inactive');
+   }
+
+   if (!isDesktopMenuClosed) {
+      desktopMenu.classList.add('inactive');
+   }
+
+   aside.classList.toggle('inactive');
 }
